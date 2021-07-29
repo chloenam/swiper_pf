32ad0dd4711db0d27e61e0db04db3a8f6df70a65
chloenam <chloe.namhjoo@gmail.com>
2021-07-29 13:25:09 +0900

    💄 Add pagination, btn style

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const menu = ["", "", "", ""];
+const menu = ["About", "Brand", "News", "Source"];
 const swiper = new Swiper("#wrap", {
   loop: true,
   slidesPerView: "auto",
@@ -23,35 +23,35 @@ const swiper = new Swiper("#wrap", {
 });
 
 //DOM Cashing
-const bg = document.querySelectorAll(".bg li");
-const prev = document.querySelector(".swiper-button-prev");
-const next = document.querySelector(".swiper-button-next");
-const navi = document.querySelectorAll(".swiper-pagination span");
+// const bg = document.querySelectorAll(".bg li");
+// const prev = document.querySelector(".swiper-button-prev");
+// const next = document.querySelector(".swiper-button-next");
+// const navi = document.querySelectorAll(".swiper-pagination span");
 
-prev.addEventListener("click", activation);
-next.addEventListener("click", activation);
-window.addEventListener("mousewheel", activation);
+// prev.addEventListener("click", activation);
+// next.addEventListener("click", activation);
+// window.addEventListener("mousewheel", activation);
 
-swiper.on("slideChangeTransitionEnd", activation);
+// swiper.on("slideChangeTransitionEnd", activation);
 
-for (let el of navi) {
-  el.addEventListener("click", (e) => {
-    const isOn = e.currentTarget.classList.contains(
-      "swiper-pagination-bullet-active"
-    );
+// for (let el of navi) {
+//   el.addEventListener("click", (e) => {
+//     const isOn = e.currentTarget.classList.contains(
+//       "swiper-pagination-bullet-active"
+//     );
 
-    if (isOn) return;
-    swiper.on("slideChangeTransitionEnd", activation);
-  });
-}
+//     if (isOn) return;
+//     swiper.on("slideChangeTransitionEnd", activation);
+//   });
+// }
 
-function activation() {
-  let item = document.querySelector(".swiper-slide-active");
-  let i = item.getAttribute("data-swiper-slide-index");
+// function activation() {
+//   let item = document.querySelector(".swiper-slide-active");
+//   let i = item.getAttribute("data-swiper-slide-index");
 
-  for (let el of bg) {
-    el.classList.remove("on");
-  }
+//   for (let el of bg) {
+//     el.classList.remove("on");
+//   }
 
-  bg[i].classList.add("on");
-}
+//   bg[i].classList.add("on");
+// }
