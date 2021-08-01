b8a9ef070980c974b6331aaaa75ca98904d2a320
chloenam <chloe.namhjoo@gmail.com>
2021-08-01 12:59:01 +0900

    🔨 Add use anime js and add motion

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ const swiper = new Swiper("#wrap", {
   spaceBetween: 150,
   mousewheel: true,
   grabCursor: true,
-  speed: 800,
 
   pagination: {
     el: ".swiper-pagination",
@@ -16,4 +15,106 @@ const swiper = new Swiper("#wrap", {
     },
     clickable: true,
   },
+
+  effect: "fade",
+});
+
+swiper.on("slideChangeTransitionStart", function () {
+  anime({
+    targets: ".swiper-slide-active .downUp",
+    translateY: [50, 0],
+    delay: anime.stagger(120, { start: 200 }),
+    opacity: [0, 1],
+    easing: "easeInOutQuart",
+  });
+
+  anime({
+    targets: ".swiper-slide-active .upDown",
+    translateY: [50, 0],
+    translateX: ["-50%", "-50%"],
+    delay: anime.stagger(120, { start: 200 }),
+    opacity: [0, 1],
+    easing: "easeInOutQuart",
+  });
+
+  anime({
+    targets: ".swiper-slide-active .leftR",
+    translateX: [-50, 0],
+    delay: 700,
+    opacity: [0, 1],
+    easing: "easeInOutQuart",
+  });
+
+  anime({
+    targets: ".swiper-slide-active .rightL",
+    translateX: [50, 0],
+    delay: 700,
+    opacity: [0, 1],
+    easing: "easeInOutQuart",
+  });
+
+  anime({
+    targets: ".swiper-slide-active .picM",
+    scale: [1.2, 1],
+    delay: 800,
+    opacity: [0, 1],
+    easing: "easeInOutQuart",
+  });
+
+  anime({
+    targets: ".swiper-slide-active.news .picD img ",
+    translateY: [50, 0],
+    delay: 0,
+    opacity: [0, 1],
+    easing: "easeInOutQuart",
+  });
+
+  anime({
+    targets: ".swiper-slide-active .picT",
+    translateY: [50, 0],
+    delay: 1000,
+    opacity: [0, 1],
+    easing: "easeInOutQuart",
+  });
+});
+
+//load effect
+anime({
+  targets: ".swiper-slide-active .downUp",
+  translateY: [50, 0],
+  delay: anime.stagger(120, { start: 200 }),
+  opacity: [0, 1],
+  easing: "easeInOutQuart",
+});
+
+anime({
+  targets: ".swiper-slide-active .leftR",
+  translateX: [-50, 0],
+  delay: 700,
+  opacity: [0, 1],
+  easing: "easeInOutQuart",
+});
+
+anime({
+  targets: ".swiper-slide-active .rightL",
+  translateX: [50, 0],
+  delay: 700,
+  opacity: [0, 1],
+  easing: "easeInOutQuart",
+});
+
+anime({
+  targets: ".swiper-slide-active .picM",
+  scale: [1.2, 1],
+  delay: 800,
+  opacity: [0, 1],
+  easing: "easeInOutQuart",
+});
+
+anime({
+  targets: ".swiper-slide-active .picT",
+  translateY: [50, 0],
+  delay: 1000,
+  opacity: [0, 1],
+  easing: "easeInOutQuart",
 });
